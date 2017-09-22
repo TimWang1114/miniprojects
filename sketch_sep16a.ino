acf7182c355061bf86e0766d88b4287a2c79a576
TimWang1114 <32003683+TimWang1114@users.noreply.github.com>
2017-09-21 20:27:12 -0400

    Add files via upload
    servo control

---
+++ sketch_sep16a.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+Servo servo1;
+void setup() {
+  // put your setup code here, to run once:
+  servo1.attach(3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int angle = analogRead(0);
+  angle = map(angle,0,1023,0,180);
+  servo1.write(angle);
+  delay(15);
+}
